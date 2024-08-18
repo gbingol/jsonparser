e88c6bafa229fcb07db447a981427f7963d4404c
gbingol <gbingol@hotmail.com>
2024-08-18 10:00:56 +0200

    using struct rather than class

--- objects.h
+++ objects.h
@@ -11,7 +11,7 @@ namespace JSON
     struct CType{};
 
     template<typename T>
-    class BasicType : public CType
+    struct BasicType : CType
     {
         BasicType(T i):m_Data{i}{}
        
@@ -42,13 +42,13 @@ namespace JSON
     };
 
 
-    class Int : public BasicType<int> { };
-    class Double : public  BasicType<double>{};
-    class String : public  BasicType<std::string>{};
-    class Bool : public  BasicType<bool>{};
+    struct Int : BasicType<int> { };
+    struct Double : BasicType<double>{};
+    struct String : BasicType<std::string>{};
+    struct Bool : BasicType<bool>{};
    
-    class Array : public  BasicType<std::vector<CType>>{};
-    class Object : public  BasicType<std::unordered_map<std::string, CType>>{};
+    struct Array : BasicType<std::vector<CType>>{};
+    struct Object : BasicType<std::unordered_map<std::string, CType>>{};
 
     //std::monostate for JSON's null
     using Value = std::variant<Int, Double, String, Bool, Array, Object>;
