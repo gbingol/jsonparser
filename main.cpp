cf6b92eaa7f9013f81914caf7519c682328ad98b
Gökhan Bingöl <gbingol@hotmail.com>
2024-08-19 16:22:38 +0200

    changed path

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 
-	JSON::JSON json(std::filesystem::path("js_manifest.json"));
+	JSON::JSON json(std::filesystem::path("jsonfiles/manifest.json"));
 	auto v = json.Parse();
 	std::cout << v;
 
